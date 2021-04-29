d291f727f62d85f469d978175aff6c388fd69caa
Jamie Hamilton <jamiehamilton81@gmail.com>
2021-04-29 12:10:12 +0100

    Added wellbeing entity and slight formatting changes to fragment_wellbeing.xml

---
+++ app/src/main/java/com/sid1804492/bottomnavtest/database/Wellbeing.kt
@@ -0,0 +1,28 @@
+package com.sid1804492.bottomnavtest.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName="wellbeing_table")
+data class Wellbeing (
+
+    @PrimaryKey(autoGenerate = true)
+    val WellbeingId: Long = 0L,
+
+    @ColumnInfo(name = "date")
+    val date: Calendar,
+
+    @ColumnInfo(name = "rating")
+    val rating: Int,
+
+    @ColumnInfo(name = "went_well")
+    val wentWell: String,
+
+    @ColumnInfo(name = "improve_on")
+    val improveOn: String,
+
+    @ColumnInfo(name = "anything_else")
+    val anythingElse: String
+)
